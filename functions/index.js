94b7a734c92cf80e85b7a1b5a74b7d404043ef6a
alermi <alermi@ucsd.edu>
2020-05-08 14:30:05 +0300

    Wrote the function to do the pairing

--- functions/index.js
+++ functions/index.js
@@ -1,17 +1,22 @@
 const functions = require('firebase-functions');
 const { App, ExpressReceiver } = require('@slack/bolt');
 const admin = require('firebase-admin');
+const shuffle = require('shuffle-array');
+
+const config = functions.config();
+const signingSecret = config.slack.signing_secret;
+const token = config.slack.token;
 
 admin.initializeApp();
 
 const expressReceiver = new ExpressReceiver({
-    signingSecret: functions.config().slack.signing_secret,
+    signingSecret: signingSecret,
     endpoints: '/events'
 });
 
 const app = new App({
     receiver: expressReceiver,
-    token: functions.config().slack.token
+    token: token
 });
 
 // Global error handler
@@ -20,15 +25,18 @@ app.error(console.log);
 // Handle `/echo` command invocations
 app.command('/pairup', async ({ command, ack, say }) => {
     // Acknowledge command request
+
     ack();
-    say(`You said "${command.text}"`);
+    say(`Trying to pair up.`);
+    pairUp();
+
 });
 app.message(async ({ message, context }) => {
     try{
         // console.log(message)
         if(message.channel_type === 'im'){
             app.client.chat.postMessage({
-                token: context.botToken,
+                token: token,
                 channel: '#general',
                 text: `<@${message.user}> just DMd me. What a creep! Other people should also know that "${message.text}"`
             });
@@ -40,3 +48,87 @@ app.message(async ({ message, context }) => {
 
 });
 exports.slack = functions.https.onRequest(expressReceiver.app);
+
+
+async function pairUp(){
+    try{
+        const {members} = await app.client.users.list({
+            token:token
+        });
+   
+        // Get the human users among all users
+        const users = Array.from(members);
+        const humans = users.filter( user => {
+            //SlackBot is includeded now for testing purposes, need to filter that out too.
+            return !user.is_bot && user.id!=='USLACKBOT';
+        });
+
+        if(humans.length <= 1){
+            console.log("Could not pair since there is less than 2 people in the workspace");
+            return;
+        }
+        var ids = humans.map( human => human.id );
+
+        // Randomize the order of people
+        shuffle(ids);
+
+        for (i = 0; i < ids.length/2; i++) {
+            
+            // var pair = new Array(ids[i], ids[i]);
+            // console.log(pair);
+            
+            var responsePromise = app.client.conversations.open({
+                token: token,
+                return_im: false,
+                users: ids[i]+','+ids[(ids.length/2) + i]
+            })
+            responsePromise.then(response => handlePairingResponse(response))
+                                .catch(console.error);
+
+        }
+
+    }
+    catch(error){
+        console.error(error);
+    }
+}
+async function handlePairingResponse(response){
+
+    if(!response.ok){
+        return console.error(response.error);
+    }
+    // app.client.conversations.join({
+    //     token: token,
+    //     channel: response.channel.id
+    // });
+    return app.client.chat.postMessage({
+        token: token,
+        channel: response.channel.id,
+        text: "You ppl just got paired!"
+    });
+}
+
+// const Firestore = require('@google-cloud/firestore');
+// const PROJECTID = 'altitest-5f53d';
+// const COLLECTION_NAME = 'Workspaces';
+// const firestore = new Firestore({
+//   projectId: PROJECTID,
+//   timestampsInSnapshots: true,
+// });
+
+
+// Firestore access example:
+
+// firestore.collection(COLLECTION_NAME)
+// .doc('T0132EDC3M4')
+// .get()
+// .then(doc => {
+//   if (!(doc && doc.exists)) {
+//     console.log({ error: 'Unable to find the document' });
+//   }
+//   const data = doc.data();
+//   console.log(data);
+// }).catch(err => {
+//   console.error(err);
+//   console.log({ error: 'Unable to retrieve the document' });
+// });
