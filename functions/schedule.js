151542f01156f4ec6edf6ba3b2172617dd93d944
Yinbai Ding <dingyinbai@megalinkware.com>
2020-05-11 09:30:30 -0700

    Trying to pull code

--- functions/schedule.js
+++ functions/schedule.js
@@ -6,12 +6,36 @@ exports.warmup = async function warmup(app, token) {
         let hour = 9;
 
         //invoke sayStuff every 2 second
-        let timerId = setInterval(sayStuff, 2000, app, token, hour);
+        //let timerId = setInterval(sayStuff, 2000, app, token, hour);
+
+        var now = new Date(),
+        start,
+        wait;
+
+        if (now.getHours() < hour) {
+            start = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 8, 0, 0, 0);
+        } else {
+            start = new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1, 8, 0, 0, 0);
+        }
+
+        wait = start.getTime() - now.getTime();
+
+        if(wait <= 0) { //If missed 8am before going into the setTimeout
+            console.log('Oops, missed the hour');
+            every8am(yourcode); //Retry
+        } else {
+            setTimeout(function () { //Wait 8am
+                setInterval(function () {
+                    yourcode();
+                }, 86400000); //Every day
+            },wait);
+        }
 
     } catch(error) {
         console.error(error);
     }
 }
+
 
 async function sayStuff(app, token, hour) {
     var d = new Date();
