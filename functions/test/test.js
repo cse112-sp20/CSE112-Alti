1778a3cb919220cb9243a5473432c50ced53b101
Yinbai Ding <dingyinbai@megalinkware.com>
2020-05-12 20:16:13 -0700

    Making individual branch

--- functions/test/test.js
+++ functions/test/test.js
@@ -1,3 +1,30 @@
+const functions = require('firebase-functions');
+const { App, ExpressReceiver } = require('@slack/bolt');
+const admin = require('firebase-admin');
+
+const config = functions.config();
+const signingSecret = config.slack.signing_secret;
+const user_token = config.slack.user_token;
+const bot_token = config.slack.bot_token;
+
+
+const expressReceiver = new ExpressReceiver({
+    signingSecret: signingSecret,
+    endpoints: '/events',
+});
+
+const app = new App({
+    receiver: expressReceiver,
+    token: bot_token
+});
+
+exports.getBolt = function getBolt(){
+    return {
+        app:app,
+        token:bot_token
+    }
+};
+
 var assert = require('assert');
 
 describe('Array', function() {
