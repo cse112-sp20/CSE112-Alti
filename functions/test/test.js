92295de500ce27073cb8f114de1e4cc4481cf9b7
Yinbai Ding <dingyinbai@megalinkware.com>
2020-05-15 17:29:37 -0700

    Commented a line

--- functions/test/test.js
+++ functions/test/test.js
@@ -10,7 +10,7 @@ describe('Scheduler', function() {
     let now = new Date();
     now.setTime(now.getTime() + 60000); 
     let response = await schedule.warmupMsgs(now.getHours(), now.getMinutes());
-    console.log(response);
+    //console.log(response);
     assert.equal(response.ok, true);
   });
 
