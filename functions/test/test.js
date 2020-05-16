299dd863f8197ca5b52d019bd90031c7b31ab08b
jerry1oo <jeremiah7847@gmail.com>
2020-05-16 14:35:45 -0700

    testing again

--- functions/test/test.js
+++ functions/test/test.js
@@ -11,7 +11,7 @@ describe('Scheduler', function() {
     now.setTime(now.getTime() + 60000); 
     let response = await schedule.scheduleMsg(now.getHours(), now.getMinutes(), 
                                                     "A reminder", "#testing");
-    //console.log(response);
+    console.log(response);
     assert.equal(response.ok, true);
   });
 
