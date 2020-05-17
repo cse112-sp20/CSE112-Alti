373adcdb7a2011c98a591cc63af15b941078d17f
Daniel Fritsch <dfritsch99@gmail.com>
2020-05-16 23:13:55 -0700

    Done with Ani's request

--- functions/firestore.js
+++ functions/firestore.js
@@ -15,8 +15,6 @@ else{
     admin.initializeApp(functions.config().firebase);
 }
 
-
-
 let db = admin.firestore();
 
 /* 
@@ -34,34 +32,6 @@ exports.storeNewPairings = function storeNewPairings(workspace, channel, pairing
                                 .collection('teammatePairings').doc(pairing).set({test: 'this is another test hi daniel'});
 }
 
-exports.firestoreTest = function firestoreTest() {
-    let docRef = db.collection('Workspaces').doc('T0132EDC3M4').get().then((doc) => {	
-        if (!(doc && doc.exists)) {	
-            return console.log({ error: 'Unable to find the document' });	
-        }	
-        console.log(String(doc.data().users));	
-        return null;
-    }).catch((err) => {	
-        return console.log('Error getting documents', err);	
-    });
-}
-
-exports.printPairingData = function testEmulator1() {
-    db.collection('workspaces').doc('workspace3').collection('activeChannels').doc('testchannel')
-                            .collection('teammatePairings').doc('235363').get()
-        .then(doc => {
-            if (!doc.exists) {
-                console.log('No such document! :(');
-            } else {
-                console.log('Document data:', doc.data());
-            }
-            return null;
-        })
-        .catch(err => {
-        console.log('Error getting document', err);
-        });
-}
-
 exports.writeMsgToDB = function writeMsgToDB(teamId, userID, channelID,msgToSend,isWarmup) {
 	db.collection("workspaces").doc(teamId+"/activeChannels/"+channelID+"/teammatePairings/"+userID).set({
 		warmupMessage: msgToSend
@@ -70,4 +40,75 @@ exports.writeMsgToDB = function writeMsgToDB(teamId, userID, channelID,msgToSend
 
 exports.storeNewPairingChannel = function storeNewPairingChannel(workspaceID, newChannel) {
     db.collection("workspaces").doc(workspaceID).collection('activeChannels').doc(newChannel).set({});
+}
+
+/* 
+    Description:
+        This function gets called when a user picks an exercise for their pair's warmup or cooldown activity.
+        The activity task prompt gets stored in the user's partner's warmup or cooldown task field.
+        This function needs to determine the given user's partner as a part of the functionality which can
+        be found in the entry for that user in teammatePairings collection. 
+
+    Input: 
+        workspaceID - workspace id
+        channelID - channel id over channel from which pairing was created
+        userID - user id of user who selected this task for their partner
+        isWarmup - (boolean) true if warmup, false if cooldown
+        exercisePrompt - for example: "_____ sent you this C++ coding speed typing test to warmup your
+                                       mind and fingers! Complete it here: https://somelink.com"
+*/
+exports.storeTypeOfExercise = async function storeTypeOfExercise(workspaceID, channelID, userID, isWarmup, exercisePrompt) {
+    let partnerID = await this.getPartner(workspaceID, channelID, userID);
+    let partnerRef = db.collection("workspaces").doc(workspaceID).collection("activeChannels").doc(channelID).collection('pairedUsers').doc(partnerID)
+
+    if (isWarmup) {
+        partnerRef.set({'warmupTask': exercisePrompt}, {merge: true});
+    }
+    else {
+        partnerRef.set({'cooldownTask': exercisePrompt}, {merge: true});
+    }
+}
+
+/*
+    Description:
+        Given a user within a pairing channel, return its partner's userID.
+    
+    Input:
+        workspaceID - workspace id
+        channelID - channel id over channel from which pairing was created
+        userID - user id of user who you want to find their respective partner
+    
+    Returns:
+        partner's userID, or undefined if error or cannot find the user passed in
+*/
+exports.getPartner = function getPartner(workspaceID, channelID, userID) {
+    let userRef = db.collection("workspaces").doc(workspaceID).collection("activeChannels")
+                    .doc(channelID).collection('pairedUsers').doc(userID);
+    
+    return userRef.get()
+        .then(doc => {
+            if (!doc.exists) {
+                console.log('No such document!');
+                return undefined;
+            }
+            else {
+                return doc.data().partnerID;
+            }
+        })
+        .catch(err => {
+            console.log('Error getting user document: ', err);
+            return undefined;
+        });
+}
+
+/*
+    Description:
+        Given a workspace and pairing channel id, return a list of lists that contain paired user IDs
+    
+    Input: 
+        workspaceID - workspace id
+        channelID - channel id of pairing channel you're looking to get paired users from
+*/
+exports.getPairedUsers = function getPairedUsers(workspaceID, channelID) {
+
 }
