87e3a7e827077566e3bbc81d86488d6c0f9ceea7
jerry1oo <jeremiah7847@gmail.com>
2020-05-19 17:30:30 -0700

    Revert "."
    This reverts commit 8719e9927bd29f7b4198e72bcb26381d04290427.

--- functions/pairUp.js
+++ functions/pairUp.js
@@ -119,6 +119,29 @@ async function handlePairingResponse(response, app, token, workspaceInfo, pairin
 }
 
 
+// Given a channel name, returns the channel ID.
+exports.getChannelIdByName = async function getChannelIdByName(app, token, channelName){
+    const conversations = app.client.conversations.list({
+        token:token
+    });
+    const channelId = conversations.then( conversations => {
+        const filteredChannels = conversations.channels.filter( channel => {
+            if(channel.name === channelName){
+                return true;
+            }
+            else return false;
+        })
+
+        if (filteredChannels.length === 0){
+            console.error("Target channel not found");
+            return undefined;
+        }
+        if (filteredChannels.length > 1){
+            console.error("Multiple channels found");
+            return undefined;
+        }
+    })
+};
 app.command('/pairup', ({ command, ack, say, context }) => {
     // Acknowledge command request
     ack();
