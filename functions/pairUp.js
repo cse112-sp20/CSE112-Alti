4c0f9448dfea363f2780fa62cb5d0fe0325b79e3
jerry1oo <jeremiah7847@gmail.com>
2020-05-19 17:20:56 -0700

    add pairup

--- functions/pairUp.js
+++ functions/pairUp.js
@@ -119,27 +119,10 @@ async function handlePairingResponse(response, app, token, workspaceInfo, pairin
 }
 
 
-// Given a channel name, returns the channel ID.
-exports.getChannelIdByName = async function getChannelIdByName(app, token, channelName){
-    const conversations = app.client.conversations.list({
-        token:token
-    });
-    const channelId = conversations.then( conversations => {
-        const filteredChannels = conversations.channels.filter( channel => {
-            if(channel.name === channelName){
-                return true;
-            }
-            else return false;
-        })
+app.command('/pairup', ({ command, ack, say, context }) => {
+    // Acknowledge command request
+    ack();
+    say(`Trying to pair up.`);
+    exports.pairUp("general", context);
 
-        if (filteredChannels.length === 0){
-            console.error("Target channel not found");
-            return undefined;
-        }
-        if (filteredChannels.length > 1){
-            console.error("Multiple channels found");
-            return undefined;
-        }
-
-    })
-};
+});
