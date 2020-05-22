4ffafe317614bd4feb06f27e87aab74c4df35d4e
alermi <alermi@ucsd.edu>
2020-05-22 10:40:57 +0300

    Revert "merged with brent testing"
    This reverts commit 550a8411775a559b6c00bff41655629c0417c30f, reversing
    changes made to 5b16a01cbf3ce94e2ad31ec7f526f906bd9fb0a0.

--- functions/test/test.js
+++ functions/test/test.js
@@ -12,10 +12,6 @@ let token = "xoxb-1109790171392-1110712837169-OxF8igcVuxkFUhbZVuoXxypj";
 
 
 
-var schedule = require('../schedule');
-const quotes = require('./quotes');
-const motivationalQuotes = quotes.getQuotesObj();
-
 // If it passes, means the function finished and message was scheduled, baseline test
 // Need more rigorous testing using promises of async function and validation from Slack API channel reading
 describe('Scheduler', () => {
@@ -257,25 +253,5 @@ describe('Setup Warmup Callbacks', () => {
       assert.equal(prompt.substring(0,expectedString.length), expectedString);
       return Promise.resolve();
     });
-  });
-});
-describe('generateQuote', () => {
-  it('Testing random quote', () => {
-    //generateCodingChallenge();
-    resultantString = generateTaskData.generateQuote();
-	for (var quoteIterator = 0; quoteIterator < motivationalQuotes.length; quoteIterator++) { 
-		let quoteText = motivationalQuotes[quoteIterator].text;
-		let quoteAuthor =  motivationalQuotes[quoteIterator].author;
-		if (quoteAuthor === null) {
-			quoteAuthor = "Unknown";
-		}
-		potentialString = quoteIterator+"-"+quoteText+" - "+ quoteAuthor;
-		if (resultantString === potentialString) {
-			assert.equal(resultantString,potentialString);
-		}
-		else if (quoteIterator === motivationalQuotes.length-1) {
-			assert.equal(resultantString,"No matching strings in quote db");
-		}
-	}
   });
 });
