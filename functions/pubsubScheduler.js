6c04984a23c14a967043003f5c3919a31435fc4e
Ruixian Song <ruixian@Clericuzios-MacBook.local>
2020-05-25 15:41:28 -0700

    add prompt funcs

--- functions/pubsubScheduler.js
+++ functions/pubsubScheduler.js
@@ -19,189 +19,204 @@ var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "S
 */
 
 exports.scheduledPairUp = functions.pubsub
-                            .schedule('every sunday 13:00')
-                            .timeZone('America/Los_Angeles')
-                            .onRun(async (context) =>  {
+														.schedule('every sunday 13:00')
+														.timeZone('America/Los_Angeles')
+														.onRun(async (context) =>  {
 
-  const allWorkspaces = await firestoreFuncs.getAllWorkspaces();
+	const allWorkspaces = await firestoreFuncs.getAllWorkspaces();
 
-  for( i=0; i<allWorkspaces.length; i++){
-    timedTask(i, allWorkspaces);
-  }
-  return null;
+	for( i=0; i<allWorkspaces.length; i++){
+		timedTask(i, allWorkspaces);
+	}
+	return null;
 });
 
 function timedTask(i, allWorkspaces){
-  setTimeout(() => {
-    const workspace = allWorkspaces[i];
-    // if (  workspace !== "T0132A75VD3" && workspace !== "T0132A75VD3" ){
-    //   return;
-    // }
-    firestoreFuncs.getAPIPair(workspace)
-    .then( res => {
-      return handleWorkspacePairup(workspace, res);
-    }).catch(err => console.error(err));
-  }, 3000 * i)
+	setTimeout(() => {
+		const workspace = allWorkspaces[i];
+		// if (  workspace !== "T0132A75VD3" && workspace !== "T0132A75VD3" ){
+		//   return;
+		// }
+		firestoreFuncs.getAPIPair(workspace)
+		.then( res => {
+			return handleWorkspacePairup(workspace, res);
+		}).catch(err => console.error(err));
+	}, 3000 * i)
 }
 async function handleWorkspacePairup(workspace, apiPair){
-        if(apiPair !== null){
-          const botToken = apiPair.botToken;
-          try{
-            const pairUpResult = pairUp.pairUp(undefined, botToken);
-            // console.log("Paired up workspace " + workspace)
-            return pairUpResult;
-          }catch(error){
-            console.error("Could not schedule pair up for workspace " + workspace +
-            ". This may be because the pairing channel might not be set up in firestore.")
-          }
+				if(apiPair !== null){
+					const botToken = apiPair.botToken;
+					try{
+						const pairUpResult = pairUp.pairUp(undefined, botToken);
+						// console.log("Paired up workspace " + workspace)
+						return pairUpResult;
+					}catch(error){
+						console.error("Could not schedule pair up for workspace " + workspace +
+						". This may be because the pairing channel might not be set up in firestore.")
+					}
 
-        }
-        else{
-          console.error("Could not schedule pair up for workspace " + workspace +
-                        " because the api pair is not stored in firestore.")
-        }
-        return Promise.resolve();
+				}
+				else{
+					console.error("Could not schedule pair up for workspace " + workspace +
+												" because the api pair is not stored in firestore.")
+				}
+				return Promise.resolve();
 }
 exports.scheduleWarmup = functions.pubsub
-                            .schedule('every mon,tue,wed,thu,fri 00:10')
-                            .timeZone('America/Los_Angeles')
-                            .onRun((context) => {
-    app.use(({context}) => schedule.scheduleMsg(9, 0, "A reminder for warmup", "#general", context.botToken));
-    
-    
+														.schedule('every mon,tue,wed,thu,fri 00:10')
+														.timeZone('America/Los_Angeles')
+														.onRun((context) => {
+		app.use(({context}) => schedule.scheduleMsg(9, 0, "A reminder for warmup", "#general", context.botToken));
+		
+		
 });
 
 exports.scheduleDaily = functions.pubsub
-                          .schedule('every mon,tue,wed,thu,fri 00:10')
-                          .timeZone('America/Los_Angeles')
-                          .onRun((context) => {
-    // Look through workspace's users in db and schedule warmup for each user
-    scheduleDailyHelper();
+													.schedule('every mon,tue,wed,thu,fri 00:10')
+													.timeZone('America/Los_Angeles')
+													.onRun((context) => {
+		// Look through workspace's users in db and schedule warmup for each user
+		schedule.scheduleWarmupChoice(23, 41,'G0140B5QM8E', 'xoxb-1104347199445-1124064737425-M8qfMiU3N5MrW2DTZ9oRWT3Q');
+		//scheduleDailyHelper();
 
 });
 
+exports.test1 = async function() {
+	console.log("test1");
+}
+
 async function scheduleDailyHelper() {
-  let workspaces = await firestoreFuncs.getAllWorkspaces();
-  console.log(workspaces);
-  /*
-  for (var w of workspaces) {
-    console.log("Workspace: " + w);
-    var p = scheduleDailyWorkspace(w);
-    
-  }
-  */
-  scheduleDailyWorkspace("T012US11G4X");
-  return null;
+	let workspaces = await firestoreFuncs.getAllWorkspaces();
+	console.log(workspaces);
+	/*
+	for (var w of workspaces) {
+		console.log("Workspace: " + w);
+		var p = scheduleDailyWorkspace(w);
+		
+	}
+	*/
+	scheduleDailyWorkspace("T012US11G4X");
+	return null;
 }
 
 // Helper function for scheduleDailyHelper
 async function scheduleDailyWorkspace(workspaceId) {
-  var d = new Date();
-  var n = d.getDay();
-  var day = days[n];
-  console.log(day);
+	var d = new Date();
+	var n = d.getDay();
+	var day = days[n];
+	console.log(day);
 
-  var keyObj = await firestoreFuncs.getAPIPair(workspaceId);
-  if (!keyObj) {
-    console.log("No API key");
-    return;
-  }
-  
-  var w_token = keyObj.botToken;
+	var keyObj = await firestoreFuncs.getAPIPair(workspaceId);
+	if (!keyObj) {
+		console.log("No API key");
+		return;
+	}
+	
+	var w_token = keyObj.botToken;
 
-  let channel = await firestoreFuncs.getPairingChannel(workspaceId);
+	let channel = await firestoreFuncs.getPairingChannel(workspaceId);
 
-  if (!channel) {
-    console.log("No pairing channel");
-    return;
-  }
-  else {
-    console.log("Pairing Channel: " + channel);
-  }
-  let convoObj = await app.client.conversations.members({
-    token: w_token,
-    channel: channel
-  }).catch((error) => {
-    console.log(error);
-  });
+	if (!channel) {
+		console.log("No pairing channel");
+		return;
+	}
+	else {
+		console.log("Pairing Channel: " + channel);
+	}
+	let convoObj = await app.client.conversations.members({
+		token: w_token,
+		channel: channel
+	}).catch((error) => {
+		console.log(error);
+	});
 
-  let memberList = convoObj.members;
-  for (var m of memberList) {
-    scheduleDailyUser(workspaceId, m, w_token, day);
-  }
+	let memberList = convoObj.members;
+	for (var m of memberList) {
+		scheduleDailyUser(workspaceId, m, w_token, day);
+	}
 
 }
 
 // Helper function for scheduleDailyWorkspace
 async function scheduleDailyUser(workspaceId, userId, token, day) {
-  day = "Monday";
-  console.log("user: " + userId);
-  let pairingData = await firestoreFuncs.getUserPairingData(workspaceId, userId);
-  let warmupTime = await firestoreFuncs.getWarmupTime(workspaceId, userId, day);
-  let cooldownTime = await firestoreFuncs.getCooldownTime(workspaceId, userId, day);
+	day = "Monday";
+	console.log("user: " + userId);
+	let pairingData = await firestoreFuncs.getUserPairingData(workspaceId, userId);
+	let warmupTime = await firestoreFuncs.getWarmupTime(workspaceId, userId, day);
+	let cooldownTime = await firestoreFuncs.getCooldownTime(workspaceId, userId, day);
 
-  if (!pairingData || !warmupTime || !cooldownTime) {
-    console.log("Incomplete user data");
-    return null;
-  }
+	if (!pairingData || !warmupTime || !cooldownTime) {
+		console.log("Incomplete user data");
+		return null;
+	}
 
-  var warmupTask;
-  var cooldownTask;
-  var dmThreadID = pairingData.dmThreadID;
+	var warmupTask;
+	var cooldownTask;
+	
 
-  if (day === "Monday") {
-    //TODO generate hardcoded tasks for monday quote and retrospective
-    warmupTask = null;
-    cooldownTask = null;
-  }
-  else {
-    warmupTask = pairingData.warmupTask;
-    cooldownTask = pairingData.cooldownTask;
-  }
+	var dmThreadID = pairingData.dmThreadID;
 
-  //TODO if no such warmup or cooldown make one for testing
-  if (!warmupTask) {
-    // add
-  }
-  if (!cooldownTask) {
-    // add
-  }
+	if (day === "Monday") {
+		//TODO generate hardcoded tasks for monday quote and retrospective
+		warmupTask = null;
+		cooldownTask = null;
+	}
+	else {
+		warmupTask = pairingData.warmupTask;
+		cooldownTask = pairingData.cooldownTask;
+	}
 
-  console.log(warmupTime);
-  console.log(cooldownTime);
-  console.log(pairingData);
+	
+	
+	//TODO if no such warmup or cooldown make one for testing
+	if (!warmupTask) {
+		// add
+	}
+	if (!cooldownTask) {
+		// add
+	}
 
-  if (dmThreadID) {
-    var hour, min, mid;
+	console.log(warmupTime);
+	console.log(cooldownTime);
+	console.log(pairingData);
 
-    split = warmupTime.split(" ");
-    hour = warmupTime.split(" ")[0].split(":")[0];
-    min = warmupTime.split(" ")[0].split(":")[1];
-    mid = warmupTime.split(" ")[1];
-    if (mid === "PM") {
-      hour = String(Number(hour) + 12);
-    }
-    console.log(hour);
-    console.log(min);
-    console.log(mid);
+	if (dmThreadID) {
+		var hour, min, mid;
 
-    schedule.scheduleMsg(hour, min, warmupTask, dmThreadID, token);
-    
+		split = warmupTime.split(" ");
+		hour = warmupTime.split(" ")[0].split(":")[0];
+		min = warmupTime.split(" ")[0].split(":")[1];
+		mid = warmupTime.split(" ")[1];
+		if (mid === "PM") {
+			hour = String(Number(hour) + 12);
+		}
+		console.log(hour);
+		console.log(min);
+		console.log(mid);
 
-    split = cooldownTime.split(" ");
-    hour = cooldownTime.split(" ")[0].split(":")[0];
-    min = cooldownTime.split(" ")[0].split(":")[1];
-    mid = cooldownTime.split(" ")[1];
+		schedule.scheduleMsg(hour, min, warmupTask, dmThreadID, token);
+		
 
-    if (mid === "PM") {
-      hour = String(Number(hour) + 12);
-    }
-    console.log(hour);
-    console.log(min);
-    console.log(mid);
+		split = cooldownTime.split(" ");
+		hour = cooldownTime.split(" ")[0].split(":")[0];
+		min = cooldownTime.split(" ")[0].split(":")[1];
+		mid = cooldownTime.split(" ")[1];
 
-    schedule.scheduleMsg(hour, min, cooldownTask, dmThreadID, token);
-  }
+		if (mid === "PM") {
+			hour = String(Number(hour) + 12);
+		}
+		console.log(hour);
+		console.log(min);
+		console.log(mid);
 
-  return null;
+		schedule.scheduleMsg(hour, min, cooldownTask, dmThreadID, token);
+
+		if (day !== 'Friday') {
+			schedule.scheduleWarmupChoice(hour, min, dmThreadID, token);
+			schedule.scheduleCooldownChoice(hour, min, dmThreadID, token);
+		}
+
+	}
+
+	return null;
 }
--- functions/schedule.js
+++ functions/schedule.js
@@ -12,33 +12,266 @@ const index = require('./index')
 const functions = require('firebase-functions');
 const app = index.getBolt();
 
-  /*
-    Description:
-        schedule a message to a channel/user at a destined time. Will be used to send
-        warmup/cooldown. This function is assumed to be called around midnight. 
-    Input: 
-        hour - the hour of the mssage to be sent
-        minute - the minute of the message
-        text - the content of message
-        id - the id of the user or channel
-    Return:
-        the response coming back from slack. 
-  */
-  exports.scheduleMsg = async function scheduleMsg(hour, minute, text, id, token) {
-    // set up the time
-    reminder = new Date();
-    reminder.setHours(hour);
-    reminder.setMinutes(minute);
-    reminder.setSeconds(0);
+	/*
+		Description:
+				schedule a message to a channel/user at a destined time. Will be used to send
+				warmup/cooldown. This function is assumed to be called around midnight. 
+		Input: 
+				hour - the hour of the mssage to be sent
+				minute - the minute of the message
+				text - the content of message
+				id - the id of the user or channel
+		Return:
+				the response coming back from slack. 
+	*/
 
-    //call api
-    return app.client.chat.scheduleMessage({
-                      token: token,
-                      channel: id,
-                      text:  text,
-                      post_at: reminder.getTime()/1000 // conversion from milli sec to sec
-                  }).catch((error) => {
-                    return error.data;
-                });
-      
-  };
+	exports.scheduleMsg = async function scheduleMsg(hour, minute, text, id, token) {
+		// set up the time
+		reminder = new Date();
+		reminder.setHours(hour);
+		reminder.setMinutes(minute);
+		reminder.setSeconds(0);
+
+		//call api
+		return app.client.chat.scheduleMessage({
+											token: token,
+											channel: id,
+											text:  text,
+											post_at: reminder.getTime()/1000 // conversion from milli sec to sec
+									}).catch((error) => {
+										return error.data;
+								});
+			
+	};
+
+	exports.scheduleCooldownChoice = async function(hour, minute, targChannelID,token){
+				// set up the time
+		reminder = new Date();
+		reminder.setHours(hour);
+		reminder.setMinutes(minute);
+		reminder.setSeconds(0);
+		const notificationString = "Send a cool-down to your buddy!"
+		//warmup selection message json
+		const cooldownSelect = [
+				{
+					"type": "section",
+					"text": {
+						"type": "plain_text",
+						"emoji": true,
+						"text": 'Which cool-down would you like to send your buddy to get them out of "the zone" this afternoon?'
+					}
+				},
+				{
+					"type": "divider"
+				},
+				{
+					"type": "section",
+					"text": {
+						"type": "mrkdwn",
+						"text": "*Options:*"
+					}
+				},
+				{
+					"type": "section",
+					"text": {
+						"type": "mrkdwn",
+						"text": "*Custom Message*"
+					},
+					"accessory": {
+						"action_id": "cooldown_custom_select",
+						"type": "button",
+						"text": {
+							"type": "plain_text",
+							"emoji": true,
+							"text": "Choose"
+						},
+						"value": "select_custom"
+					}
+				},
+				{
+					"type": "section",
+					"text": {
+						"type": "mrkdwn",
+						"text": "*Retrospective Questions*"
+					},
+					"accessory": {
+						"action_id": "cooldown_retro_select",
+						"type": "button",
+						"text": {
+							"type": "plain_text",
+							"emoji": true,
+							"text": "Choose"
+						},
+						"value": "select_retro"
+					}
+				},
+				{
+					"type": "section",
+					"text": {
+						"type": "mrkdwn",
+						"text": "*Non-tech Article*"
+					},
+					"accessory": {
+						"action_id": "cooldown_article_select",
+						"type": "button",
+						"text": {
+							"type": "plain_text",
+							"emoji": true,
+							"text": "Choose"
+						},
+						"value": "select_article"
+					}
+				},
+				{
+					"type": "section",
+					"text": {
+						"type": "mrkdwn",
+						"text": "*Non-tech Video*"
+					},
+					"accessory": {
+						"action_id": "cooldown_video_select", 
+						"type": "button",
+						"text": {
+							"type": "plain_text",
+							"emoji": true,
+							"text": "Choose"
+						},
+						"value": "select_video"
+					}
+				}
+			];
+		//try function logic
+			return app.client.chat.scheduleMessage({
+				token: token,
+				channel: targChannelID,
+				text:  notificationString,
+				blocks: cooldownSelect,
+				post_at: reminder.getTime()/1000 
+			}).catch((error) => {
+				return error.data;
+		});
+	}
+
+	exports.scheduleWarmupChoice = async function(hour, minute,targChannelID,token){
+		reminder = new Date();
+		reminder.setHours(hour);
+		reminder.setMinutes(minute);
+		reminder.setSeconds(0);
+		const notificationString = "Send a warmup to your buddy!"
+		//warmup selection message json
+		const warmupSelect = [
+				{
+					"type": "section",
+					"text": {
+						"type": "plain_text",
+						"emoji": true,
+						"text": "Hey there, pick a warmup for your buddy!"
+					}
+				},
+				{
+					"type": "divider"
+				},
+				{
+					"type": "section",
+					"text": {
+						"type": "mrkdwn",
+						"text": "*Pick a content type:*"
+					}
+				},
+				{
+					"type": "section",
+					"text": {
+						"type": "mrkdwn",
+						"text": "*Speed Typing Test*"
+					},
+					"accessory": {
+						"action_id": "warmup_coding_select",
+						"type": "button",
+						"text": {
+							"type": "plain_text",
+							"emoji": true,
+							"text": "Choose"
+						},
+						"value": "select_test"
+					}
+				},
+				{
+					"type": "section",
+					"text": {
+						"type": "mrkdwn",
+						"text": "*Tech Article*"
+					},
+					"accessory": {
+						"action_id": "warmup_article_select",
+						"type": "button",
+						"text": {
+							"type": "plain_text",
+							"emoji": true,
+							"text": "Choose"
+						},
+						"value": "select_article"
+					}
+				},
+				{
+					"type": "section",
+					"text": {
+						"type": "mrkdwn",
+						"text": "*Easy Online Puzzle*"
+					},
+					"accessory": {
+						"action_id": "warmup_puzzle_select",
+						"type": "button",
+						"text": {
+							"type": "plain_text",
+							"emoji": true,
+							"text": "Choose"
+						},
+						"value": "select_puzzle"
+					}
+				},
+				{
+					"type": "section",
+					"text": {
+						"type": "mrkdwn",
+						"text": "*Motivational Quote*"
+					},
+					"accessory": {
+						"action_id": "warmup_quote_select", 
+						"type": "button",
+						"text": {
+							"type": "plain_text",
+							"emoji": true,
+							"text": "Choose"
+						},
+						"value": "select_quote"
+					}
+				},
+				 {
+					"type": "section",
+					"text": {
+						"type": "mrkdwn",
+						"text": "*Custom Message*"
+					},
+					"accessory": {
+						"action_id": "request_custom_send", 
+						"type": "button",
+						"text": {
+							"type": "plain_text",
+							"emoji": true,
+							"text": "Choose"
+						},
+						"value": "select_custom"
+					}
+				}
+			];
+		//try function logic
+		return app.client.chat.scheduleMessage({
+				token: token,
+				channel: targChannelID,
+				text:  notificationString,
+				blocks: warmupSelect,
+				post_at: reminder.getTime()/1000 
+			}).catch((error) => {
+				return error.data;
+		});
+	}
