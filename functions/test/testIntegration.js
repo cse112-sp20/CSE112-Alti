f5aa591841477a39cd142dde54eeaf02e040e199
jerry1oo <jeremiah7847@gmail.com>
2020-06-03 14:50:03 -0700

    change timezone to utc

--- functions/test/testIntegration.js
+++ functions/test/testIntegration.js
@@ -66,12 +66,21 @@ describe('Integration Testing', () => {
       assert(postAtTime-scheduleTime < 5000, "The schedule time does not match with input"); // allow 5 sec delay
     });
 
-    it('schedule for 2 min before', async () => {
+    it('schedule for 2 min before', async function() {
+      this.timeout(5000);
+      let initial = new Date();
       let now = new Date();
+      var localTime = now.getTime();
+      let localOffset = now.getTimezoneOffset()*60000;
+      let utc = localTime + localOffset;
+      let offset = -7;
+      let cali = (utc + (3600000 * offset));
+      let newDate = new Date(cali);
+      now = newDate;
       now.setTime(now.getTime() - 120000); 
       let response = await schedule.scheduleMsg(now.getHours(), now.getMinutes(), 
                                                       "A failed reminder", "#testing", token);
-      console.log(response)
+      //console.log(response)
       assert.equal(response.ok, false);
     });
 
